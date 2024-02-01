20b5aa064107f96d4586dc8b01fe4bf1fd229c76
Shota Ide <ide.shota.dev@gmail.com>
2024-02-01 12:38:45 +0900

    style: fix

--- Sources/ExtractDocComments/TestClass.swift
+++ Sources/ExtractDocComments/TestClass.swift
@@ -5,7 +5,7 @@ public struct TestClass: Hashable {
     let docComment: DocComment?
 
     public static func == (lhs: TestClass, rhs: TestClass) -> Bool {
-        return lhs.name == rhs.name && lhs.docComment == rhs.docComment
+        lhs.name == rhs.name && lhs.docComment == rhs.docComment
     }
 
     public func hash(into hasher: inout Hasher) {
