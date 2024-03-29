0a61fa5814a7c0d1eef0fd4307f21574fe6813b1
Shota Ide <shotaIDE@users.noreply.github.com>
2024-03-29 23:22:42 +0900

    refactor: add comments on empty method's body (#24)

--- Tests/XCTestDocProjectGenTests/ExampleUITests/BobTests.swift
+++ Tests/XCTestDocProjectGenTests/ExampleUITests/BobTests.swift
@@ -7,10 +7,14 @@ class BobTests: XCTestCase {
     /// Test case 01 for Alice
     ///
     /// This is the description of test case 01 for Bob.
-    func testBob01() {}
+    func testBob01() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 
     /// Test case 02 for Bob
     ///
     /// This is the description of test case 02 for Bob.
-    func testBob02() {}
+    func testBob02() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 }
