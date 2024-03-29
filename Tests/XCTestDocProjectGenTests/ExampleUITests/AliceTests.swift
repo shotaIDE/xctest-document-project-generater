4a5140225d7ca9d9b6559fb9cb6e7824b8992d94
Shota Ide <ide.shota.dev@gmail.com>
2024-03-29 23:19:51 +0900

    refactor: add comments on empty method body

--- Tests/XCTestDocProjectGenTests/ExampleUITests/AliceTests.swift
+++ Tests/XCTestDocProjectGenTests/ExampleUITests/AliceTests.swift
@@ -7,10 +7,14 @@ class AliceTests: XCTestCase {
     /// Test case 01 for Alice
     ///
     /// This is the description of test case 01 for Alice.
-    func testAlice01() {}
+    func testAlice01() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 
     /// Test case 02 for Alice
     ///
     /// This is the description of test case 02 for Alice.
-    func testAlice02() {}
+    func testAlice02() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 }
--- Tests/XCTestDocProjectGenTests/ExampleUITests/SubFolder/CarolTests.swift
+++ Tests/XCTestDocProjectGenTests/ExampleUITests/SubFolder/CarolTests.swift
@@ -7,10 +7,14 @@ class CarolTests: XCTestCase {
     /// Test case 01 for Carol
     ///
     /// This is the description of test case 01 for Carol.
-    func testCarol01() {}
+    func testCarol01() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 
     /// Test case 02 for Carol
     ///
     /// This is the description of test case 02 for Carol.
-    func testCarol02() {}
+    func testCarol02() {
+        // No content because the gen tool only checks the method name and the modifier.
+    }
 }
